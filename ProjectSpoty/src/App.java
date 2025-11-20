22596d045074c92b21438f81e720b5982b664f7b
Carlos Vazquez <carlosangel2710@hotmail.com>
2025-11-19 23:44:27 -0600

    Conexión a la base de datos

--- ProjectSpoty/src/App.java
+++ ProjectSpoty/src/App.java
@@ -1,5 +1,21 @@
+import java.sql.*;
+import Config.Con;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //* Menus */
+
+        //* Conexión a la base de datos */
+        Con con = null;
+        Connection connection = null;
+        try {
+            connection = con.getConn();
+            System.out.println("Conexión correcta");
+        } catch (SQLException e) {
+            System.err.println("Error de conexión: " + e.getMessage());
+        } finally {
+            con.closeConnetion(connection);
+        }
     }
 }
+
